cff1035c73df8e2354b63f411d5869adedceacf4
João Pedro <jpmns9103@gmail.com>
2021-12-16 13:01:38 -0300

    atualizar depedências e sintaxes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import "dotenv/config";
 
 const api = axios.create({
-  baseURL: "process.env.REACT_APP_API_URL",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Authorization": `Bearer ${localStorage.getItem("token")}`
   }
